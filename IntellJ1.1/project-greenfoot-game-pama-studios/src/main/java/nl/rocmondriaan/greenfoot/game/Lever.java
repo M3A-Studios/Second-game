bf1f131f64804dca3e86d8c1d9558f98dbdc94c4
Patryk <Patryk9720@hotmail.com>
2019-11-14 10:24:15 +0100

    Auto stash before merge of "master" and "origin/master"

--- IntellJ1.1/project-greenfoot-game-pama-studios/src/main/java/nl/rocmondriaan/greenfoot/game/Lever.java
+++ IntellJ1.1/project-greenfoot-game-pama-studios/src/main/java/nl/rocmondriaan/greenfoot/game/Lever.java
@@ -2,35 +2,75 @@ package nl.rocmondriaan.greenfoot.game;
 
 import greenfoot.Greenfoot;
 
+import greenfoot.*;
+
 public class Lever extends Blocks
 {
+    // Animation en cd timers
+    private int cooldownTimer = 0;
+    private int animationTimer;
+
+    // Lever Images
+    GreenfootImage links = new GreenfootImage("233.png");
+    GreenfootImage middle = new GreenfootImage ("234.png");
+    GreenfootImage rechts = new GreenfootImage ("235.png");
     /**
      * Calls for the constructor in Blocks.java to set the image of the tile
      *
      * @param ID    ID used to get what image this block should use
      */
-    private int cdTimer = 100;
-
     Lever(int ID) {
         super(ID);
     }
 
     public void act()
     {
-        if (cdTimer == 0)
-        {
-            if (Greenfoot.isKeyDown(Options.interact)) {
+        // Methodes voor Animatie en lever systeem
+        leverAnim();
+        leverSys();
 
-            }
-
-        }
-        else
-        {
-            cdTimer --;
-        }
+        //Timer
+        if (cooldownTimer > 0)
+        { cooldownTimer--; }
 
     }
+    public void leverSys()
+    {
 
+        if (Greenfoot.isKeyDown(Options.interact))
+        {
+            animationTimer ++;
+            if (cooldownTimer <= 0)
+            {
+                animationTimer = 0;
+                setImage(middle);
+                middle.scale((Options.blockSize),(Options.blockSize));
+                cooldownTimer = 100;
+            }
+        }
+    }
+    public void leverAnim()
+    {
+
+        if (animationTimer== 30)
+        {
+            setImage(middle);
+            middle.scale((Options.blockSize),(Options.blockSize));
+        }
+        if (animationTimer== 60)
+        {
+            setImage(rechts);
+            rechts.scale((Options.blockSize),(Options.blockSize));
+        }
+    }
+    public boolean isLinks()
+    {
+        if()
+        {
+            return true;
+        }
+        return false;
+    }
 }
 
 
