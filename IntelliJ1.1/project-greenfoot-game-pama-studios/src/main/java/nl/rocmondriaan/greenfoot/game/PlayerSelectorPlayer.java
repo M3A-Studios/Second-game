30e2da547c66bbbf8f77e198751db8dc1dfe31d5
daakubotto <alexanderborgers@outlook.com>
2019-11-20 15:22:19 +0100

    Tiny patch to new game button not reseting score/coins total
    and no, it doesnt reset your unlocked levels. thats done for development and is disabled in line 35 of PlayerSelectorPlayer.java

--- IntelliJ1.1/project-greenfoot-game-pama-studios/src/main/java/nl/rocmondriaan/greenfoot/game/PlayerSelectorPlayer.java
+++ IntelliJ1.1/project-greenfoot-game-pama-studios/src/main/java/nl/rocmondriaan/greenfoot/game/PlayerSelectorPlayer.java
@@ -29,6 +29,9 @@ public class PlayerSelectorPlayer extends Actor {
         {
             LevelSelector.setSelectedLevel(1);
             Globals.levelCoinsCollected = 0;
+            Globals.totalCoinsCollected = 0;
+            Globals.totalScore = 0;
+            Globals.levelScore = 0;
             //Globals.levelsUnlocked = 1;
             Options.player1Color = this.color;
             Greenfoot.setWorld(new LevelSelector());
