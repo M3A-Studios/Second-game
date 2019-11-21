35d6c228d5eb0b6ad3e456e6ceae1aa0b7e739f2
ashitosh <ashi_ragh@hotmail.com>
2019-11-21 05:37:55 -0800

    stuffz I guess

---
+++ target/classes/images/Spritesheet_map.tsx
@@ -0,0 +1,568 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Spritesheet_map" tilewidth="64" tileheight="64" tilecount="188" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="mapTile_001.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="mapTile_002.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="mapTile_003.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="mapTile_004.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="mapTile_005.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="mapTile_006.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="mapTile_007.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="mapTile_008.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="mapTile_009.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="mapTile_010.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="mapTile_011.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="mapTile_012.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="mapTile_013.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="mapTile_014.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="mapTile_015.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="mapTile_016.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="mapTile_017.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="mapTile_018.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="mapTile_019.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="mapTile_020.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="mapTile_021.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="mapTile_022.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="mapTile_023.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="mapTile_024.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="mapTile_025.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="mapTile_026.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="mapTile_027.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="mapTile_028.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="mapTile_029.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="mapTile_030.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="mapTile_031.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="mapTile_032.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="mapTile_033.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="mapTile_034.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="mapTile_035.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="mapTile_036.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="mapTile_037.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="mapTile_038.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="mapTile_039.png"/>
+ </tile>
+ <tile id="39">
+  <image width="64" height="64" source="mapTile_040.png"/>
+ </tile>
+ <tile id="40">
+  <image width="64" height="64" source="mapTile_041.png"/>
+ </tile>
+ <tile id="41">
+  <image width="64" height="64" source="mapTile_042.png"/>
+ </tile>
+ <tile id="42">
+  <image width="64" height="64" source="mapTile_043.png"/>
+ </tile>
+ <tile id="43">
+  <image width="64" height="64" source="mapTile_044.png"/>
+ </tile>
+ <tile id="44">
+  <image width="64" height="64" source="mapTile_045.png"/>
+ </tile>
+ <tile id="45">
+  <image width="64" height="64" source="mapTile_046.png"/>
+ </tile>
+ <tile id="46">
+  <image width="64" height="64" source="mapTile_047.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="64" source="mapTile_048.png"/>
+ </tile>
+ <tile id="48">
+  <image width="64" height="64" source="mapTile_049.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="mapTile_050.png"/>
+ </tile>
+ <tile id="50">
+  <image width="64" height="64" source="mapTile_051.png"/>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="mapTile_052.png"/>
+ </tile>
+ <tile id="52">
+  <image width="64" height="64" source="mapTile_053.png"/>
+ </tile>
+ <tile id="53">
+  <image width="64" height="64" source="mapTile_054.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="mapTile_055.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="mapTile_056.png"/>
+ </tile>
+ <tile id="56">
+  <image width="64" height="64" source="mapTile_057.png"/>
+ </tile>
+ <tile id="57">
+  <image width="64" height="64" source="mapTile_058.png"/>
+ </tile>
+ <tile id="58">
+  <image width="64" height="64" source="mapTile_059.png"/>
+ </tile>
+ <tile id="59">
+  <image width="64" height="64" source="mapTile_060.png"/>
+ </tile>
+ <tile id="60">
+  <image width="64" height="64" source="mapTile_061.png"/>
+ </tile>
+ <tile id="61">
+  <image width="64" height="64" source="mapTile_062.png"/>
+ </tile>
+ <tile id="62">
+  <image width="64" height="64" source="mapTile_063.png"/>
+ </tile>
+ <tile id="63">
+  <image width="64" height="64" source="mapTile_064.png"/>
+ </tile>
+ <tile id="64">
+  <image width="64" height="64" source="mapTile_065.png"/>
+ </tile>
+ <tile id="65">
+  <image width="64" height="64" source="mapTile_066.png"/>
+ </tile>
+ <tile id="66">
+  <image width="64" height="64" source="mapTile_067.png"/>
+ </tile>
+ <tile id="67">
+  <image width="64" height="64" source="mapTile_068.png"/>
+ </tile>
+ <tile id="68">
+  <image width="64" height="64" source="mapTile_069.png"/>
+ </tile>
+ <tile id="69">
+  <image width="64" height="64" source="mapTile_070.png"/>
+ </tile>
+ <tile id="70">
+  <image width="64" height="64" source="mapTile_071.png"/>
+ </tile>
+ <tile id="71">
+  <image width="64" height="64" source="mapTile_072.png"/>
+ </tile>
+ <tile id="72">
+  <image width="64" height="64" source="mapTile_073.png"/>
+ </tile>
+ <tile id="73">
+  <image width="64" height="64" source="mapTile_074.png"/>
+ </tile>
+ <tile id="74">
+  <image width="64" height="64" source="mapTile_075.png"/>
+ </tile>
+ <tile id="75">
+  <image width="64" height="64" source="mapTile_076.png"/>
+ </tile>
+ <tile id="76">
+  <image width="64" height="64" source="mapTile_077.png"/>
+ </tile>
+ <tile id="77">
+  <image width="64" height="64" source="mapTile_078.png"/>
+ </tile>
+ <tile id="78">
+  <image width="64" height="64" source="mapTile_079.png"/>
+ </tile>
+ <tile id="79">
+  <image width="64" height="64" source="mapTile_080.png"/>
+ </tile>
+ <tile id="80">
+  <image width="64" height="64" source="mapTile_081.png"/>
+ </tile>
+ <tile id="81">
+  <image width="64" height="64" source="mapTile_082.png"/>
+ </tile>
+ <tile id="82">
+  <image width="64" height="64" source="mapTile_083.png"/>
+ </tile>
+ <tile id="83">
+  <image width="64" height="64" source="mapTile_084.png"/>
+ </tile>
+ <tile id="84">
+  <image width="64" height="64" source="mapTile_085.png"/>
+ </tile>
+ <tile id="85">
+  <image width="64" height="64" source="mapTile_086.png"/>
+ </tile>
+ <tile id="86">
+  <image width="64" height="64" source="mapTile_087.png"/>
+ </tile>
+ <tile id="87">
+  <image width="64" height="64" source="mapTile_088.png"/>
+ </tile>
+ <tile id="88">
+  <image width="64" height="64" source="mapTile_089.png"/>
+ </tile>
+ <tile id="89">
+  <image width="64" height="64" source="mapTile_090.png"/>
+ </tile>
+ <tile id="90">
+  <image width="64" height="64" source="mapTile_091.png"/>
+ </tile>
+ <tile id="91">
+  <image width="64" height="64" source="mapTile_092.png"/>
+ </tile>
+ <tile id="92">
+  <image width="64" height="64" source="mapTile_093.png"/>
+ </tile>
+ <tile id="93">
+  <image width="64" height="64" source="mapTile_094.png"/>
+ </tile>
+ <tile id="94">
+  <image width="64" height="64" source="mapTile_095.png"/>
+ </tile>
+ <tile id="95">
+  <image width="64" height="64" source="mapTile_096.png"/>
+ </tile>
+ <tile id="96">
+  <image width="64" height="64" source="mapTile_097.png"/>
+ </tile>
+ <tile id="97">
+  <image width="64" height="64" source="mapTile_098.png"/>
+ </tile>
+ <tile id="98">
+  <image width="64" height="64" source="mapTile_099.png"/>
+ </tile>
+ <tile id="99">
+  <image width="64" height="64" source="mapTile_100.png"/>
+ </tile>
+ <tile id="100">
+  <image width="64" height="64" source="mapTile_101.png"/>
+ </tile>
+ <tile id="101">
+  <image width="64" height="64" source="mapTile_102.png"/>
+ </tile>
+ <tile id="102">
+  <image width="64" height="64" source="mapTile_103.png"/>
+ </tile>
+ <tile id="103">
+  <image width="64" height="64" source="mapTile_104.png"/>
+ </tile>
+ <tile id="104">
+  <image width="64" height="64" source="mapTile_105.png"/>
+ </tile>
+ <tile id="105">
+  <image width="64" height="64" source="mapTile_106.png"/>
+ </tile>
+ <tile id="106">
+  <image width="64" height="64" source="mapTile_107.png"/>
+ </tile>
+ <tile id="107">
+  <image width="64" height="64" source="mapTile_108.png"/>
+ </tile>
+ <tile id="108">
+  <image width="64" height="64" source="mapTile_109.png"/>
+ </tile>
+ <tile id="109">
+  <image width="64" height="64" source="mapTile_110.png"/>
+ </tile>
+ <tile id="110">
+  <image width="64" height="64" source="mapTile_111.png"/>
+ </tile>
+ <tile id="111">
+  <image width="64" height="64" source="mapTile_112.png"/>
+ </tile>
+ <tile id="112">
+  <image width="64" height="64" source="mapTile_113.png"/>
+ </tile>
+ <tile id="113">
+  <image width="64" height="64" source="mapTile_114.png"/>
+ </tile>
+ <tile id="114">
+  <image width="64" height="64" source="mapTile_115.png"/>
+ </tile>
+ <tile id="115">
+  <image width="64" height="64" source="mapTile_116.png"/>
+ </tile>
+ <tile id="116">
+  <image width="64" height="64" source="mapTile_117.png"/>
+ </tile>
+ <tile id="117">
+  <image width="64" height="64" source="mapTile_118.png"/>
+ </tile>
+ <tile id="118">
+  <image width="64" height="64" source="mapTile_119.png"/>
+ </tile>
+ <tile id="119">
+  <image width="64" height="64" source="mapTile_120.png"/>
+ </tile>
+ <tile id="120">
+  <image width="64" height="64" source="mapTile_121.png"/>
+ </tile>
+ <tile id="121">
+  <image width="64" height="64" source="mapTile_122.png"/>
+ </tile>
+ <tile id="122">
+  <image width="64" height="64" source="mapTile_123.png"/>
+ </tile>
+ <tile id="123">
+  <image width="64" height="64" source="mapTile_124.png"/>
+ </tile>
+ <tile id="124">
+  <image width="64" height="64" source="mapTile_125.png"/>
+ </tile>
+ <tile id="125">
+  <image width="64" height="64" source="mapTile_126.png"/>
+ </tile>
+ <tile id="126">
+  <image width="64" height="64" source="mapTile_127.png"/>
+ </tile>
+ <tile id="127">
+  <image width="64" height="64" source="mapTile_128.png"/>
+ </tile>
+ <tile id="128">
+  <image width="64" height="64" source="mapTile_129.png"/>
+ </tile>
+ <tile id="129">
+  <image width="64" height="64" source="mapTile_130.png"/>
+ </tile>
+ <tile id="130">
+  <image width="64" height="64" source="mapTile_131.png"/>
+ </tile>
+ <tile id="131">
+  <image width="64" height="64" source="mapTile_132.png"/>
+ </tile>
+ <tile id="132">
+  <image width="64" height="64" source="mapTile_133.png"/>
+ </tile>
+ <tile id="133">
+  <image width="64" height="64" source="mapTile_134.png"/>
+ </tile>
+ <tile id="134">
+  <image width="64" height="64" source="mapTile_135.png"/>
+ </tile>
+ <tile id="135">
+  <image width="64" height="64" source="mapTile_136.png"/>
+ </tile>
+ <tile id="136">
+  <image width="64" height="64" source="mapTile_137.png"/>
+ </tile>
+ <tile id="137">
+  <image width="64" height="64" source="mapTile_138.png"/>
+ </tile>
+ <tile id="138">
+  <image width="64" height="64" source="mapTile_139.png"/>
+ </tile>
+ <tile id="139">
+  <image width="64" height="64" source="mapTile_140.png"/>
+ </tile>
+ <tile id="140">
+  <image width="64" height="64" source="mapTile_141.png"/>
+ </tile>
+ <tile id="141">
+  <image width="64" height="64" source="mapTile_142.png"/>
+ </tile>
+ <tile id="142">
+  <image width="64" height="64" source="mapTile_143.png"/>
+ </tile>
+ <tile id="143">
+  <image width="64" height="64" source="mapTile_144.png"/>
+ </tile>
+ <tile id="144">
+  <image width="64" height="64" source="mapTile_145.png"/>
+ </tile>
+ <tile id="145">
+  <image width="64" height="64" source="mapTile_146.png"/>
+ </tile>
+ <tile id="146">
+  <image width="64" height="64" source="mapTile_147.png"/>
+ </tile>
+ <tile id="147">
+  <image width="64" height="64" source="mapTile_148.png"/>
+ </tile>
+ <tile id="148">
+  <image width="64" height="64" source="mapTile_149.png"/>
+ </tile>
+ <tile id="149">
+  <image width="64" height="64" source="mapTile_150.png"/>
+ </tile>
+ <tile id="150">
+  <image width="64" height="64" source="mapTile_151.png"/>
+ </tile>
+ <tile id="151">
+  <image width="64" height="64" source="mapTile_152.png"/>
+ </tile>
+ <tile id="152">
+  <image width="64" height="64" source="mapTile_153.png"/>
+ </tile>
+ <tile id="153">
+  <image width="64" height="64" source="mapTile_154.png"/>
+ </tile>
+ <tile id="154">
+  <image width="64" height="64" source="mapTile_155.png"/>
+ </tile>
+ <tile id="155">
+  <image width="64" height="64" source="mapTile_156.png"/>
+ </tile>
+ <tile id="156">
+  <image width="64" height="64" source="mapTile_157.png"/>
+ </tile>
+ <tile id="157">
+  <image width="64" height="64" source="mapTile_158.png"/>
+ </tile>
+ <tile id="158">
+  <image width="64" height="64" source="mapTile_159.png"/>
+ </tile>
+ <tile id="159">
+  <image width="64" height="64" source="mapTile_160.png"/>
+ </tile>
+ <tile id="160">
+  <image width="64" height="64" source="mapTile_161.png"/>
+ </tile>
+ <tile id="161">
+  <image width="64" height="64" source="mapTile_162.png"/>
+ </tile>
+ <tile id="162">
+  <image width="64" height="64" source="mapTile_163.png"/>
+ </tile>
+ <tile id="163">
+  <image width="64" height="64" source="mapTile_164.png"/>
+ </tile>
+ <tile id="164">
+  <image width="64" height="64" source="mapTile_165.png"/>
+ </tile>
+ <tile id="165">
+  <image width="64" height="64" source="mapTile_166.png"/>
+ </tile>
+ <tile id="166">
+  <image width="64" height="64" source="mapTile_167.png"/>
+ </tile>
+ <tile id="167">
+  <image width="64" height="64" source="mapTile_168.png"/>
+ </tile>
+ <tile id="168">
+  <image width="64" height="64" source="mapTile_169.png"/>
+ </tile>
+ <tile id="169">
+  <image width="64" height="64" source="mapTile_170.png"/>
+ </tile>
+ <tile id="170">
+  <image width="64" height="64" source="mapTile_171.png"/>
+ </tile>
+ <tile id="171">
+  <image width="64" height="64" source="mapTile_172.png"/>
+ </tile>
+ <tile id="172">
+  <image width="64" height="64" source="mapTile_173.png"/>
+ </tile>
+ <tile id="173">
+  <image width="64" height="64" source="mapTile_174.png"/>
+ </tile>
+ <tile id="174">
+  <image width="64" height="64" source="mapTile_175.png"/>
+ </tile>
+ <tile id="175">
+  <image width="64" height="64" source="mapTile_176.png"/>
+ </tile>
+ <tile id="176">
+  <image width="64" height="64" source="mapTile_177.png"/>
+ </tile>
+ <tile id="177">
+  <image width="64" height="64" source="mapTile_178.png"/>
+ </tile>
+ <tile id="178">
+  <image width="64" height="64" source="mapTile_179.png"/>
+ </tile>
+ <tile id="179">
+  <image width="64" height="64" source="mapTile_180.png"/>
+ </tile>
+ <tile id="180">
+  <image width="64" height="64" source="mapTile_181.png"/>
+ </tile>
+ <tile id="181">
+  <image width="64" height="64" source="mapTile_182.png"/>
+ </tile>
+ <tile id="182">
+  <image width="64" height="64" source="mapTile_183.png"/>
+ </tile>
+ <tile id="183">
+  <image width="64" height="64" source="mapTile_184.png"/>
+ </tile>
+ <tile id="184">
+  <image width="64" height="64" source="mapTile_185.png"/>
+ </tile>
+ <tile id="185">
+  <image width="64" height="64" source="mapTile_186.png"/>
+ </tile>
+ <tile id="186">
+  <image width="64" height="64" source="mapTile_187.png"/>
+ </tile>
+ <tile id="187">
+  <image width="64" height="64" source="mapTile_188.png"/>
+ </tile>
+</tileset>
