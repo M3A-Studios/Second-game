444dc0c051b5526932bdc0b65048f2c3e64204c3
Michael <MichaelCheungZakelijk@gmail.com>
2019-11-27 15:26:31 +0100

    Added Image + new level 1 + update tileset

--- IntelliJ1.1/project-greenfoot-game-pama-studios/src/main/java/game/Globals.java
+++ IntelliJ1.1/project-greenfoot-game-pama-studios/src/main/java/game/Globals.java
@@ -26,14 +26,16 @@ public class Globals
     public static int totalScore;
 
     //Used in the levels and levelSelector class to know what ID is what kind of block with the check method there.
-    public static Integer[] platforms = {66,67,68,69,84,85,86,87,102,103,104,105,120,121,122,123,138,139,140,141,156,157,158,159,208,209};
+    public static Integer[] platforms = {66,67,68,69,84,85,86,87,102,103,104,105,120,121,122,123,138,139,140,141,156,
+            157,158,159,208,209};
     public static Integer[] slopeLefts = {70,88,106,124,142,160};
     public static Integer[] slopeRights = {71,89,107,125,143,161};
     public static Integer[] lavas = {222,223,224};
     public static Integer[] waters = {};
     public static Integer[] spikes = {261,262,263,264};
     public static Integer[] finishFlag = {270, 271,272};
-    public static Integer[] nonSolids = {210,211,212,215,216,217,218,219,232,233,234,235,236,237,238,239,240,269,270,260,265,266,267,268};
+    public static Integer[] nonSolids = {210,211,212,215,216,217,218,219,232,233,234,235,236,237,238,239,240,269,270,
+            260,265,266,267,268,290,291,345,346,347,348,349};
     public static Integer[] animatedObjects = {252,253,254};
     public static Integer[] ladder = {220,221};
     public static Integer[] lever = {225,226,227};
@@ -45,7 +47,8 @@ public class Globals
     public static Integer[] door = {213,214,215,216};
     public static Integer[] jumpPad = {242,243};
     public static Integer[] lockedBlocks = {228,229,230,231,275,276,277,278};
-    public static Integer[] breakableBlocks = {192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207};
+    public static Integer[] breakableBlocks = {192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,341,342,
+            343,344};
     public static Integer[] slime = {36,37,38,39,40,41,42,43,44,45,46,47};
     public static Integer[] bee = {3,4,5};
     public static Integer[] pswitch = {273,274,194}; //273 and 274 is the power switch, the rest of je IDs change when the switch is turned on.
