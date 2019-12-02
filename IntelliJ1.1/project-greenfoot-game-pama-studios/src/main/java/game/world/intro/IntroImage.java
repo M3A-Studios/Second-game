2742005b6278631f774698850fd4871b91205044
Daaku <alexanderborgers@outlook.com>
2019-12-02 20:13:20 +0100

    Introscherm voor Micheal

---
+++ IntelliJ1.1/project-greenfoot-game-pama-studios/src/main/java/game/world/intro/IntroImage.java
@@ -0,0 +1,28 @@
+package game.world.intro;
+
+import game.Options;
+import game.world.levelselector.LevelSelector;
+import game.world.menu.Menu;
+import greenfoot.Actor;
+import greenfoot.Greenfoot;
+import greenfoot.GreenfootImage;
+
+public class IntroImage extends Actor {
+
+    private int introLength = 300;
+
+    IntroImage(String image, int width, int height) {
+        GreenfootImage introImage  = new GreenfootImage(image);
+        introImage.scale(width, height);
+        setImage(introImage);
+    }
+    public void act() {
+        introLength -= 1;
+        if (introLength <= 170) {
+            getImage().setTransparency(introLength * 3 / 2);
+        }
+        if (introLength == 0) {
+            Greenfoot.setWorld(new Menu());
+        }
+    }
+}
